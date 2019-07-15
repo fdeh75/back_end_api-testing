e9084d3e5d1053a3f5726455fd6ef5cd76de00cf
fdeh75 <fdeh75@gmail.com>
2019-07-15 22:29:46 +0300

    add graphql api

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from aiohttp import web
+from graphene import ObjectType, String
+from aiohttp_graphql import GraphQLView
+from graphene import Schema
+import graphene
 
 
-async def hello(request: web.Request):
-    data = request.query
-    print(data)
-    return web.Response(text=f"key = {data.get('key', 'default value')}, key1 = {data.get('key1', 'default data 1')}")
+class Person(ObjectType):
+    first_name = String()
+    last_name = String()
+    full_name = String()
+
+    # def resolve_first_name(parent, info):
+    #     return "Иван"
+    #
+    # def resolve_full_name(parent, info):
+    #     return 'Иванов Иван'
 
 
+class Queries(graphene.ObjectType):
+
+    person = graphene.Field(Person, first_name=graphene.String(), last_name=graphene.String())
+
+    def resolve_person(self, *args, **kwargs):
+        result = Person()
+        result.first_name = kwargs['first_name']
+        result.last_name = kwargs['last_name']
+        result.full_name = f"{kwargs['last_name']} {kwargs['first_name']}"
+
+        return result
+
+
+Schema = Schema(query=Queries)
 app = web.Application()
-app.add_routes([web.get('/', hello)])
 
+GraphQLView.attach(app, schema=Schema, graphiql=True)
 web.run_app(app, port=5002)
